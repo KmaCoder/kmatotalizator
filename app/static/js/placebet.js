79f5725cc480d2cb29ea536fe7575e49f84d04c6
Maksym <maxim4110@gmail.com>
2019-05-08 01:00:47 +0300

    random selection and clear selection on front

--- app/static/js/placebet.js
+++ app/static/js/placebet.js
@@ -2,25 +2,6 @@ $(function () {
     const $formEvents = $("#form-events");
     const $formPlaceBet = $("#form-placebet");
 
-    function placeBet(json) {
-        $.ajax({
-            type: "POST",
-            url: "/play/placebet",
-            data: JSON.stringify(json),
-            contentType: "application/json; charset=utf-8",
-            dataType: "json",
-            success: function (data) {
-                // showAlert(data['message'], "success");
-                // $formEvents[0].reset();
-                // $formPlaceBet[0].reset();
-                location.reload()
-            },
-            error: function (errMsg) {
-                showAlert(errMsg['responseJSON']['message'], "danger");
-            }
-        })
-    }
-
     $formPlaceBet.on('submit', function (e) {
         e.preventDefault();
         let json = {
@@ -29,6 +10,33 @@ $(function () {
         };
         placeBet(json)
     });
+
+    $(".js-parlay-random").click(function () {
+        randomChoice();
+    });
+
+    $(".js-parlay-clear").click(function () {
+        clearAll();
+    });
+
+    function placeBet(json) {
+        $.ajax({
+            type: "POST",
+            url: "/play/placebet",
+            data: JSON.stringify(json),
+            contentType: "application/json; charset=utf-8",
+            dataType: "json",
+            success: function (data) {
+                showAlert(data['message'], "success");
+                $formEvents[0].reset();
+                $formPlaceBet[0].reset();
+                // location.reload()
+            },
+            error: function (errMsg) {
+                showAlert(errMsg['responseJSON']['message'], "danger");
+            }
+        })
+    }
 
     function getFormData($form) {
         var unindexed_array = $form.serializeArray();
@@ -42,8 +50,13 @@ $(function () {
     }
 
     function randomChoice() {
-        $formEvents.find('input').each(function () {
-
+        $formEvents.find('.event-outcomes').each(function () {
+            let $inputs = $(this).find("input");
+            $($inputs[Math.floor(Math.random() * $inputs.length)]).prop("checked", true)
         });
+    }
+
+    function clearAll() {
+        $formEvents.find('input').prop("checked", false);
     }
 });
